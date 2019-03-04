713ab38bf8f19a5dd387d0a77b31c8d6ca92e6e0
Sage <sagevanhatten4@gmail.com>
2019-03-04 09:01:56 -0800

    added author achive

--- page-archive.php
+++ page-archive.php
@@ -24,6 +24,20 @@ get_header(); ?>
 			<?php while ( have_posts() ) : the_post(); ?>
 
 			<?php endwhile; ?>
+			<div class="post-archives">
+                    <h2>Quote Authors</h2>
+                    <ul>
+                        <?php $posts = get_posts('posts_per_page=-1');
+                        foreach ($posts as $post) : setup_postdata($post); ?>
+                            <li>
+                                <a href="<?php the_permalink(); ?>">
+                                    <?php the_title(); ?>
+                                </a>
+                            </li>
+                        <?php endforeach;
+                        wp_reset_postdata(); ?>
+                    </ul>
+                </div>
 
 			<?php the_posts_navigation(); ?>
 
